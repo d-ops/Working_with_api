ad635a91abd9c9c38d1041999433d5842e33eb1b
Samat Galyautdinov <annexm@ya.ru>
2019-08-10 17:08:22 +0300

    added file opening

--- yandex_translate.py
+++ yandex_translate.py
@@ -1,5 +1,5 @@
 import requests
-#f = open('for_ya_tr.txt')
+import json
 key = 'trnsl.1.1.20190810T120830Z.6a818d641975acd7.7b759f840552a061735768d37db6b50020747699'
 url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 def translate_me(mytext):
@@ -16,9 +16,24 @@ def translate_me(mytext):
         'lang':'ru-en',
     }
 #https://translate.yandex.net/api/v1.5/tr.json/translate?key=
-    response = requests.post(url,params = params)
-    return response.json()
+   # response = requests.post(url,params = params)
+   # return response.json()
 
-json = translate_me('привет, друг')
+
+#json = translate_me('привет, друг')
+#print(json)
+#print(json['text'])
+f = open('for_ya_tr')
+
+def new_file_translator(f):
+    text = f.read()
+    params = {
+        'key': key,
+        'text': text,
+        'lang': 'ru-en',
+    }
+    response = requests.post(url, params = params)
+    return response.json()
+json = new_file_translator(f)
 print(json)
 print(json['text'])
